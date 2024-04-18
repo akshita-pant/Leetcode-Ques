ddbecff87e283227ac7ced7736270b3596a99915
Akshita Pant <103747723+akshita-pant@users.noreply.github.com>
2024-04-18 17:18:42 +0530

    Update 0048-rotate-image.cpp

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -17,3 +17,10 @@ public:
        }
     }
 };
+
+
+/*
+we iterate over the elements of the matrix and swap each element (i, j) with the element (j, i). However, if we were to start j from 0 in the inner loop, we would end up swapping each element twice, effectively restoring the original matrix.
+
+So, why does the loop for j start from i? It's because we only need to transpose the upper triangular part of the matrix (above the main diagonal). Starting j from i ensures that we only swap the elements above the main diagonal, avoiding redundant swaps.
+*/
